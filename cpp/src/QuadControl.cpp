ef896d17b74ad3ab91164c6ccb9be5cf534fd150
binliu-base <2598725694@qq.com>
2018-04-14 23:33:40 +0800

    update scenario 2

--- cpp/src/QuadControl.cpp
+++ cpp/src/QuadControl.cpp
@@ -49,6 +49,9 @@ void QuadControl::Init()
   //added by binliu 180413
   kappa = config->Get(_config + ".kappa", 0);
   l = L / sqrt(2.f);
+  Ixx = config->Get(_config + ".Ixx", 0);
+  Iyy = config->Get(_config + ".Iyy", 0);
+  Izz = config->Get(_config + ".Izz", 0);
 
 #else
   // load params from PX4 parameter system
@@ -75,18 +78,26 @@ VehicleCommand QuadControl::GenerateMotorCommands(float collThrustCmd, V3F momen
 
   ////////////////////////////// BEGIN STUDENT CODE ///////////////////////////
 
-  //cmd.desiredThrustsN[0] = mass * 9.81f / 4.f; // front left
-  //cmd.desiredThrustsN[1] = mass * 9.81f / 4.f; // front right
-  //cmd.desiredThrustsN[2] = mass * 9.81f / 4.f; // rear left
-  //cmd.desiredThrustsN[3] = mass * 9.81f / 4.f; // rear right
 	float tor_x = momentCmd.x;
 	float tor_y = momentCmd.y;
 	float tor_z = -momentCmd.z/kappa;
 
-	float f1 = (collThrustCmd + tor_x / l + tor_y / l + tor_z ) / 4.f;
-	float f2 = (collThrustCmd - tor_x / l + tor_y / l - tor_z ) / 4.f;
-	float f3 = (collThrustCmd - tor_x / l - tor_y / l + tor_z) / 4.f;
-	float f4 = (collThrustCmd + tor_x / l - tor_y / l - tor_z) / 4.f; 
+	float f1 = (collThrustCmd + tor_x / l + tor_y / l + tor_z ) / 4.f;   // front left
+	float f2 = (collThrustCmd - tor_x / l + tor_y / l - tor_z ) / 4.f;   // front right
+	float f3 = (collThrustCmd - tor_x / l - tor_y / l + tor_z) / 4.f;    // rear left 
+	float f4 = (collThrustCmd + tor_x / l - tor_y / l - tor_z) / 4.f;    // rear right
+
+	//f1 = (f1 < minMotorThrust) ? minMotorThrust
+	//	: (f1 > maxMotorThrust) ? maxMotorThrust : f1;
+
+	//f2 = (f2 < minMotorThrust) ? minMotorThrust
+	//	: (f2 > maxMotorThrust) ? maxMotorThrust : f2;
+
+	//f3 = (f3 < minMotorThrust) ? minMotorThrust
+	//	: (f3 > maxMotorThrust) ? maxMotorThrust : f3;
+
+	//f4 = (f4 < minMotorThrust) ? minMotorThrust
+	//	: (f4 > maxMotorThrust) ? maxMotorThrust : f4;
 
 	cmd.desiredThrustsN[0] = f1; // front left
 	cmd.desiredThrustsN[1] = f2; // front right
@@ -94,7 +105,6 @@ VehicleCommand QuadControl::GenerateMotorCommands(float collThrustCmd, V3F momen
 	cmd.desiredThrustsN[3] = f3; // rear right
 
 	/////////////////////////////// END STUDENT CODE ////////////////////////////
-
   return cmd;
 }
 
@@ -115,24 +125,9 @@ V3F QuadControl::BodyRateControl(V3F pqrCmd, V3F pqr)
   V3F momentCmd;
 
   ////////////////////////////// BEGIN STUDENT CODE ///////////////////////////
-  float p = pqr[0];
-  float q = pqr[1];
-  float r = pqr[2];
 
-  float p_c = pqrCmd[0];
-  float q_c = pqrCmd[1];
-  float r_c = pqrCmd[2];
-
-  float p_err = p_c - p;
-  float u_bar_p = kpPQR[0] * p_err;
-
-  float q_err = q_c - q;
-  float u_bar_q = kpPQR[1] * q_err;
-
-  float r_err = r_c - r;
-  float u_bar_r = kpPQR[2] * r_err;
-
-  momentCmd = V3F(u_bar_p, u_bar_q, u_bar_r);
+  V3F I = V3F(Ixx, Iyy, Izz);
+  momentCmd = I * kpPQR * (pqrCmd - pqr);
 
 
   /////////////////////////////// END STUDENT CODE ////////////////////////////
@@ -143,28 +138,51 @@ V3F QuadControl::BodyRateControl(V3F pqrCmd, V3F pqr)
 // returns a desired roll and pitch rate 
 V3F QuadControl::RollPitchControl(V3F accelCmd, Quaternion<float> attitude, float collThrustCmd)
 {
-  // Calculate a desired pitch and roll angle rates based on a desired global
-  //   lateral acceleration, the current attitude of the quad, and desired
-  //   collective thrust command
-  // INPUTS: 
-  //   accelCmd: desired acceleration in global XY coordinates [m/s2]
-  //   attitude: current or estimated attitude of the vehicle
-  //   collThrustCmd: desired collective thrust of the quad [N]
-  // OUTPUT:
-  //   return a V3F containing the desired pitch and roll rates. The Z
-  //     element of the V3F should be left at its default value (0)
+	// Calculate a desired pitch and roll angle rates based on a desired global
+	//   lateral acceleration, the current attitude of the quad, and desired
+	//   collective thrust command
+	// INPUTS: 
+	//   accelCmd: desired acceleration in global XY coordinates [m/s2]
+	//   attitude: current or estimated attitude of the vehicle
+	//   collThrustCmd: desired collective thrust of the quad [N]
+	// OUTPUT:
+	//   return a V3F containing the desired pitch and roll rates. The Z
+	//     element of the V3F should be left at its default value (0)
 
-  // HINTS: 
-  //  - we already provide rotation matrix R: to get element R[1,2] (python) use R(1,2) (C++)
-  //  - you'll need the roll/pitch gain kpBank
-  //  - collThrustCmd is a force in Newtons! You'll likely want to convert it to acceleration first
+	// HINTS: 
+	//  - we already provide rotation matrix R: to get element R[1,2] (python) use R(1,2) (C++)
+	//  - you'll need the roll/pitch gain kpBank
+	//  - collThrustCmd is a force in Newtons! You'll likely want to convert it to acceleration first
 
-  V3F pqrCmd;
-  Mat3x3F R = attitude.RotationMatrix_IwrtB();
+	V3F pqrCmd;
+	Mat3x3F R = attitude.RotationMatrix_IwrtB();
 
-  ////////////////////////////// BEGIN STUDENT CODE ///////////////////////////
+	////////////////////////////// BEGIN STUDENT CODE ///////////////////////////
+	float b_x = R(0, 2);
+	float b_x_c = accelCmd[0];
+	float b_x_dot_c = kpBank * (b_x_c - b_x);
 
+	float b_y = R(1, 2);
+	float b_y_c = accelCmd[1];
+	float b_y_dot_c = kpBank * (b_y_c - b_y);
 
+	Mat3x3F rot_mat;
+	rot_mat(0, 0) = R(1, 0);
+	rot_mat(0, 1) = -R(0, 0);
+	rot_mat(1, 0) = R(1, 1);
+	rot_mat(1, 1) = -R(0, 1);
+
+	rot_mat = rot_mat / R(2, 2);
+
+	V3F b_dot_Cmd;
+	b_dot_Cmd.x = b_x_dot_c;
+	b_dot_Cmd.y = b_y_dot_c;
+	b_dot_Cmd.z = 0;
+
+	V3F rot_rate = rot_mat * b_dot_Cmd;
+
+	pqrCmd.x = rot_rate.x;
+	pqrCmd.y = rot_rate.y;
 
   /////////////////////////////// END STUDENT CODE ////////////////////////////
 
